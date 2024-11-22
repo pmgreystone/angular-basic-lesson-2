932a5c44db141d87ddebef0b719e5d86fdfac94c
Prateek Mehta <pattmehta.official@gmail.com>
2024-11-21 20:21:22 -0800

    adds child routes to home component using router outlet

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,9 +4,19 @@ import { LoginComponent } from './components/login/login.component';
 import { HomeComponent } from './components/home/home.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
+import { FeedComponent } from './components/home/feed/feed.component';
+import { ReelComponent } from './components/home/reel/reel.component';
+import { SearchComponent } from './components/home/search/search.component';
+
+const homeRoutes: Routes = [
+    { path: 'feed', component: FeedComponent },
+    { path: 'reel', component: ReelComponent },
+    { path: 'search', component: SearchComponent }
+];
+
 export const routes: Routes = [
     { path: '', component: LoginComponent },
     { path: 'login', component: LoginComponent },
-    { path: 'home', component: HomeComponent },
+    { path: 'home', component: HomeComponent, children: homeRoutes },
     { path: '**', component: PageNotFoundComponent }
 ];
