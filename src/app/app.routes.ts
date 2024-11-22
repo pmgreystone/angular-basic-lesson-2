40fc38fcb3e09bbb1ebb568ca582482d355eda76
Prateek Mehta <pattmehta.official@gmail.com>
2024-11-21 19:48:28 -0800

    adds empty path for default route, wildcard path for 404 route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,11 @@ import { Routes } from '@angular/router';
 
 import { LoginComponent } from './components/login/login.component';
 import { HomeComponent } from './components/home/home.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
 export const routes: Routes = [
+    { path: '', component: LoginComponent },
     { path: 'login', component: LoginComponent },
-    { path: 'home', component: HomeComponent }
+    { path: 'home', component: HomeComponent },
+    { path: '**', component: PageNotFoundComponent }
 ];
